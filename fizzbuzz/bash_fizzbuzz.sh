62d97242b860dcb9930cbbeca41740e6f2255ffe
rhyspowell <rhys@sawdit.co.uk>
2012-03-23 11:03:08 +0000

    bash fizz buzz completed

--- fizzbuzz/bash_fizzbuzz.sh
+++ fizzbuzz/bash_fizzbuzz.sh
@@ -2,7 +2,7 @@
 
 for i in $(seq 1 100);
 do
-	if [ $[$i % 3] -eq 0 ] && [ $[$i % 5] -eq 0 ]]; then
+	if [ $[$i % 3] -eq 0 ] && [ $[$i % 5] -eq 0 ]; then
 		echo "FizzBuzz";
 	elif [ $[$i % 3] -eq 0 ]; then
 		echo "Fizz";
