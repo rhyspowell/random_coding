7398457045b1b3695b759cf917b67c174e402643
Rhys Powell <rhys.powell@gmail.com>
2015-05-14 09:04:23 +0100

    Added Python. Why oh why did it takes so long??

---
+++ fizzbuzz/python_fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+for n in range(1000):
+    if n % 3 == 0 and n % 5 ==0:
+        print "Fizz Buzz"
+    elif n%3 == 0:
+        print "Fizz"
+    elif n % 5 == 0:
+        print "Buzz"
+    else:
+        print n
